c7a88c84679f8bbf9c63b7d687df47393dfc6a12
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2023-04-13 12:50:49 +0900

    投稿フォームの設置、ログインユーザーのつぶやき登録をしました

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,4 +10,11 @@ class PostsController extends Controller
     public function index(){
         return view('posts.index');
     }
+
+    public function tweet(Request $request)
+    {
+       $post = $request->input('newPost');
+       Post::create(['post' => $post]);
+       return redirect('/top');
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+<div class="container">
+    {!! Form::open(['url' => 'post/top']) !!}
+    <div class="form-group">
+     {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください。']) !!}
+    </div>
+    <button type="submit" class="btn btn-success pull-right">送信</button>
+    {!! Form::close() !!}
+</div>
 
 @endsection
