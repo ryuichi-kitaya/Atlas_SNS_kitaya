c0f1da397c0c0769956a411cf8ad40bff12de696
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2023-05-02 12:30:03 +0900

    ユーザー検索フォームを実装しました。

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.login')
 
 @section('content')
-
-
+<div>
+  <form action="/search" method="GET">
+    @csrf
+    <input type="text" name="keyword" value="">
+    <input type="submit" value="検索">
+  </form>
+</div>
 
 @endsection
