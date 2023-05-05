05e8af062876bafed897cab54d1d24d9dbf5733c
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2023-05-05 18:38:19 +0900

    ユーザー検索機能を実装するための記述を記載しました

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Post;
 
 class UsersController extends Controller
 {
@@ -11,9 +12,12 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(Request $request){ //ユーザーを表示させるための記述
-        $user = Auth::user()->id;
-        $user = $request->input('username');
+    public function search(Request $request){ 
+        $keyword = $request ->input('keyword');
+        $query = Post::query();
+        if(!empty($keyword)){
+            $query->where('username', 'LIKE', "%{$keyword}%");
+        }
         return view('users.search');
     }
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -4,14 +4,10 @@
 <div>
   <form action="/search" method="GET">
     @csrf
-    <input type="text" name="keyword" value="">
+    <input type="text" name="keyword" value="{{$keyword}}">
     <input type="submit" value="検索">
   </form>
-  @foreach($user as $user)
-  <tr>
-    <td>{{$user->username}}</td>
-  </tr>
-  @endforeach
+  
 </div>
 
 @endsection
