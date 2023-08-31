124e5c1cdf9fd7f6cd3f01ec98255dd5b74870de
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2023-08-31 17:09:45 +0900

    ログイン画面のレイアウト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,18 +5,32 @@
 
 {!! Form::open() !!}
 
-<div class="login">
+<div class="login-container">
+<div class="login-form">
 <p>AtlasSNSへようこそ</p>
-
-{{ Form::label('e-mail') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
+<div class="mail-pass">
+<div class="login-mail">
+<p>
+{{ Form::label('mail adress') }}
+</p>
+</div>
+{{ Form::text('mail',null,['class' => 'login']) }}
+<div class="login-password">
+<p>
 {{ Form::label('password') }}
-{{ Form::password('password',['class' => 'input']) }}
-
-{{ Form::submit('ログイン') }}
-
+</p>
+</div>
+{{ Form::password('password',['class' => 'login']) }}
+</div>
+</div>
+<div class="login-btn">
+<p>
+{{ Form::submit('LOGIN',['class'=>'loginbtn']) }}
+</p>
+</div>
+<div class="register-btn">
 <p><a href="/register">新規ユーザーの方はこちら</a></p>
-
+</div>
 {!! Form::close() !!}
 </div>
 
